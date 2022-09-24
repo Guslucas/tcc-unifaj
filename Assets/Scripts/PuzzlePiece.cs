e5520515fb971cc88266a4a379f7f44d63d2064b
Gustavo Lucas da Rosa <gustavolucasdarosa@gmail.com>
2022-09-24 18:46:57 -0300

    Implemented Object Fit Puzzle (Animals)

---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    private Vector3 RightPosition;
+    public bool InRightPosition;
+    public bool Selected;
+    void Start()
+    {
+        RightPosition = transform.position;
+        transform.position = new Vector3(Random.Range(-9.28f, 9.25f), Random.Range(0.23f, -4.46f));
+    }
+    
+    void Update()
+    {
+        if (Selected)
+            Debug.Log("Distance from right position: " + Vector3.Distance(transform.position, RightPosition));
+        if (Vector3.Distance(transform.position, RightPosition) < .5f)
+        {
+            if (!Selected)
+            {
+                if (InRightPosition == false)
+                {
+                    transform.position = RightPosition;
+                    InRightPosition = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                    Camera.main.GetComponent<DragAndDrop_>().PlacedPieces++;
+                }
+            }
+        }
+    }
+}
