19b82ecf8328dcdff783912d37bb2c73d8e9080b
Gustavo Lucas da Rosa <gustavolucasdarosa@gmail.com>
2022-10-03 20:16:49 -0300

    Added AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+
+    public static AudioManager instance;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(instance);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("victory");
+    }
+
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sound: '" + s.name + "' not found.");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    // For default values
+    private void Reset()
+    {
+        sounds = new Sound[]
+        {
+            new Sound()
+        };
+    }
+
+
+}
